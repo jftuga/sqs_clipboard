942681ce3d4d2bfe4a69d7309ed42ab5a13dcf47
John Taylor <johntaylor@gmail.com>
2020-11-23 09:47:59 -0500

    used to generate a unique MessageDeduplicationId

---
+++ randString/randstring.go
@@ -0,0 +1,28 @@
+// Adapted from:
+// https://www.calhoun.io/creating-random-strings-in-go/
+
+package randstring
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func stringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+// generate a random string of given length using only the characters given in charset
+func String(length int) string {
+	return stringWithCharset(length, charset)
+}
