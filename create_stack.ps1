239c734468396193354d512ae401442e3c7a12e3
John Taylor <johntaylor@gmail.com>
2020-11-29 23:44:49 -0500

    remove debug info

--- create_stack.ps1
+++ create_stack.ps1
@@ -13,8 +13,6 @@ $templateName="cloud_formation_template.json"
 
 aws cloudformation create-stack --stack-name $sName --parameters ParameterKey=qName,ParameterValue=$qName --template-body "file://./$templateName"
 
-#$sName="sqsClipboard20201129215855"
-
 $i=0
 while($i -lt 15) {
     $qURL=(aws cloudformation describe-stacks --stack-name $sName --query "Stacks[0].Outputs[?OutputKey=='QueueURL'].OutputValue" --output text) -join "`n"
